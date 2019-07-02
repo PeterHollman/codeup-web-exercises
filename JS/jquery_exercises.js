84f482fe498f85751d3704a07e633ec3ce118f0b
Peter Hollman <peter@Peters-MacBook-Air.local>
2019-07-02 12:23:40 -0500

    second jquery exercise

---
+++ JS/jquery_exercises.js
@@ -0,0 +1,10 @@
+// $("#thediv").css("background-color", "papayawhip");
+//
+// $("#theList").css("background-color", "yellow");
+// $('*').css('border', '1px solid #F00');
+
+$("li").css("font-size", "20px");
+$("li, h1, p").css("background-color", "yellow");
+$(".theTitle").each(function() {
+    alert("Things you cannot do");
+});
