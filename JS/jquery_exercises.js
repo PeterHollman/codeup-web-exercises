90fec661e132920492947d4680efee9e675ec470
Peter Hollman <peter@Peters-MacBook-Air.local>
2019-07-02 15:19:24 -0500

    third JS exercise

--- JS/jquery_exercises.js
+++ JS/jquery_exercises.js
@@ -3,8 +3,23 @@
 // $("#theList").css("background-color", "yellow");
 // $('*').css('border', '1px solid #F00');
 
-$("li").css("font-size", "20px");
-$("li, h1, p").css("background-color", "yellow");
-$(".theTitle").each(function() {
-    alert("Things you cannot do");
+// $("li").css("font-size", "20px");
+// $("li, h1, p").css("background-color", "yellow");
+// $(".theTitle").each(function() {
+//     alert("Things you cannot do");
+// });
+
+$("h1").click(function(){
+        $(this).css('background-color', 'blue');
+});
+
+$("p").dblclick(function(){
+    $(this).css("font-size", "18px")
+});
+
+$("li").mouseover(function(){
+    $(this).css("color", "red")
+});
+$("li").mouseleave(function(){
+    $(this).css("color", "black")
 });
