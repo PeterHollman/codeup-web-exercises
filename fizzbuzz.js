1545f3c2225d87b1c16af659af55bcca1967e79a
Peter Hollman <peterjhollman@gmail.com>
2019-07-22 09:23:10 -0500

    morning commit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,22 @@
 "use strict";
 for (var i = 1; i < 101; i++) {
     if(i % 5 == 0 && i % 3 == 0){
-        console.log("FizzBuzz");
+        console.log("NeilBuzz");
     }
     else if(i % 3 == 0){
-        console.log("Fizz");
+        console.log("Neil");
     }
     else if(i % 5 == 0){
         console.log("Buzz");
     }
     else{
         console.log(i);
+    }
+}
+
+j = 1;
+for(j of iterable){
+    if(j % 5 == 0 && j % 3 == 0) {
+        console.log("NeilBuzz");
     }
 }
